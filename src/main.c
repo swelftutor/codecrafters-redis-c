f30fd3a8467643f117fdc799f6988ce74a1c1da2
swelftutor <longlongtuantuan5@gmail.com>
2025-03-12 18:23:54 +0700

    [2nnd]

--- src/main.c
+++ src/main.c
@@ -6,64 +6,85 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+
 #define BUFFER_SIZE 1024
+
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
-	setbuf(stderr, NULL);
-	
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+    // Disable output buffering
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
 
-	// Uncomment this block to pass the first stage
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    printf("Logs from your program will appear here!\n");
+
+    // Uncomment this block to pass the first stage
+
+    int server_fd, client_fd, client_addr_len;
+    struct sockaddr_in client_addr;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        printf("Socket creation failed: %s...\n", strerror(errno));
+        return 1;
+    }
+
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+                                     .sin_port = htons(6379),
+                                     .sin_addr = { htonl(INADDR_ANY) },
+                                   };
+
+    if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+        printf("Bind failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        printf("Listen failed: %s \n", strerror(errno));
+        return 1;
+    }
+
+    printf("Waiting for a client to connect...\n");
+    client_addr_len = sizeof(client_addr);
 	
-	int server_fd, client_addr_len
-	struct socketaddr_in client_addr;
-	
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-	
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockout(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(6379),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-	
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
-	printf("Client connected\n");
-	
-	int client_fd = 
-			accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+	while(1) {
+        client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+		if (client_fd == -1) {
+			printf("Accept failed: %s\n", strerror(errno));
+			continue;
+		}
+        printf("Client connected\n");
+
+        char buffer[BUFFER_SIZE];
+        ssize_t bytes_read;
 		
-			char buffer[BUFFER_SIZE];
-			while (read(client_fd, buffer, BUFFER_SIZE) != 0) {
-			  send(client_fd, "+PONG\r\n", 7, 0);
-			}
+        while ((bytes_read = read(client_fd, buffer, BUFFER_SIZE)) > 0) {
+          //  buffer[bytes_read] = '\0';  // Null-terminate the buffer
+          //  printf("Received: %s\n", buffer);
 
-	
-	close(server_fd);
+            // Respond with "+PONG\r\n"
+             if (send(client_fd, "+PONG\r\n", 7, 0) == -1)
+             {
+                perror("send failed");
+                break;
+             }
+        }
 
-	return 0;
+        if (bytes_read == -1) {
+            printf("Read failed: %s\n", strerror(errno));
+        }
+        close(client_fd);
+	}
+
+    close(server_fd);
+
+    return 0;
 }
